6acd24a0ed5626c68ac640d99ceff69eb8c7e69e
irig106 <irig106@133a2ce8-d050-0410-a33c-a8f588bb09b4>
2005-12-28 14:18:20 +0000

    Added support for GCC

--- trunk/irig106/irig106lib/src/stdint.h
+++ trunk/irig106/irig106lib/src/stdint.h
@@ -1,12 +1,13 @@
 #ifndef _stdint_h
 #define _stdint_h
 
-/*
-typedef unsigned char       uint8_t;
-typedef unsigned short      uint16_t;
-typedef unsigned long       uint32_t;
-*/
+// Modern versions of GCC usually have stdint.h so include it instead
+#if defined(__GNUC__)
+#include <stdint.h>
+#endif
 
+// Define specific sized variables for MSVC
+#if defined(_WIN32)
 typedef __int8              int8_t;
 typedef __int16             int16_t;
 typedef __int32             int32_t;
@@ -16,5 +17,6 @@ typedef unsigned __int8     uint8_t;
 typedef unsigned __int16    uint16_t;
 typedef unsigned __int32    uint32_t;
 typedef unsigned __int64    uint64_t;
+#endif
 
 #endif
