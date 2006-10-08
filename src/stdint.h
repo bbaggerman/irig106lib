acbec9dbdc2c56d5d039c51265e91693dd4a6dc5
bbaggerman <bbaggerman@133a2ce8-d050-0410-a33c-a8f588bb09b4>
2006-10-08 16:26:15 +0000

    Added support for DJGPP 32 bit DOS compiler

--- src/stdint.h
+++ src/stdint.h
@@ -2,8 +2,21 @@
 #define _stdint_h
 
 // Modern versions of GCC usually have stdint.h so include it instead
-#if defined(__GNUC__)
+#if defined(__GNUC__) && !defined(__DJGPP__)
 #include <stdint.h>
+#endif
+
+// The DJGPP 
+#if defined(__DJGPP__)
+typedef char                int8_t;
+typedef short               int16_t;
+typedef int                 int32_t;
+typedef long long           int64_t;
+
+typedef unsigned char       uint8_t;
+typedef unsigned short      uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;
 #endif
 
 // Define specific sized variables for MSVC
