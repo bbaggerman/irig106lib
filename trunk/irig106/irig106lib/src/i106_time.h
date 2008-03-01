49d53dbcb90523e57ad1001864e084395ab5984b
bbaggerman <bbaggerman@133a2ce8-d050-0410-a33c-a8f588bb09b4>
2008-03-01 04:13:44 +0000

    Time format bug fix Nifty new time to string function

--- trunk/irig106/irig106lib/src/i106_time.h
+++ trunk/irig106/irig106lib/src/i106_time.h
@@ -33,12 +33,6 @@
  (including negligence or otherwise) arising in any way out of the use 
  of this software, even if advised of the possibility of such damage.
 
- Created by Bob Baggerman
-
- $RCSfile: i106_time.h,v $
- $Date: 2006-11-20 04:36:20 $
- $Revision: 1.2 $
-
  ****************************************************************************/
 
 #ifndef _I106_TIME_H
@@ -55,6 +49,11 @@ extern "C" {
  * ----------------------
  */
 
+typedef enum
+    {
+    I106_DATEFMT_DAY         =  0,
+    I106_DATEFMT_DMY         =  1,
+    } EnI106DateFmt;
 
 
 /*
@@ -70,9 +69,11 @@ extern "C" {
 // a new time representation.  So there.
 typedef struct 
     {
-    uint32_t        ulSecs;    // This is a time_t
-    uint32_t        ulFrac;    // LSB = 100ns
+    uint32_t        ulSecs;     // This is a time_t
+    uint32_t        ulFrac;     // LSB = 100ns
+    EnI106DateFmt   enFmt;      // Day or DMY format
     } SuIrig106Time;
+
 
 // Relative time to absolute time reference
 typedef struct 
@@ -80,6 +81,64 @@ typedef struct
     uint64_t        uRelTime;          // Relative time from header
     SuIrig106Time   suIrigTime;        // Clock time from IRIG source
     } SuTimeRef;
+
+
+/// IRIG 106 secondary header time in Ch 4 BCD format
+typedef PUBLIC struct SuI106Ch4_BCD_Time_S
+    {
+    uint16_t      uMin1     : 4;    // High order time
+    uint16_t      uMin10    : 3;
+    uint16_t      uHour1    : 4;
+    uint16_t      uHour10   : 2;
+    uint16_t      uDay1     : 3;
+    uint16_t      uSec0_01  : 4;    // Low order time
+    uint16_t      uSec0_1   : 4;
+    uint16_t      uSec1     : 4;
+    uint16_t      uSec10    : 2;
+    uint16_t      uReserved : 2;
+    uint16_t      uUSecs;           // Microsecond time
+#if !defined(__GNUC__)
+    } SuI106Ch4_BCD_Time;
+#else
+    } __attribute__ ((packed)) SuI106Ch4_BCD_Time;
+#endif
+
+
+/// IRIG 106 secondary header time in Ch 4 binary format
+typedef PUBLIC struct SuI106Ch4_Binary_Time_S
+    {
+    uint16_t      uHighBinTime;     // High order time
+    uint16_t      uLowBinTime;      // Low order time
+    uint16_t      uUSecs;           // Microsecond time
+#if !defined(__GNUC__)
+    } SuI106Ch4_Binary_Time;
+#else
+    } __attribute__ ((packed)) SuI106Ch4_Binary_Time;
+#endif
+
+
+/// IRIG 106 secondary header time in IEEE-1588 format
+typedef PUBLIC struct SuIEEE1588_Time_S
+    {
+    uint32_t      uNanoSeconds;     // Nano-seconds
+    uint32_t      uSeconds;         // Seconds
+#if !defined(__GNUC__)
+    } SuIEEE1588_Time;
+#else
+    } __attribute__ ((packed)) SuIEEE1588_Time;
+#endif
+
+
+/// Intra-packet header relative time counter format
+typedef PUBLIC struct SuIntraPacketRtc_S
+    {
+    uint8_t       aubyRefTime[6];   // Reference time
+    uint16_t      uReserved;
+#if !defined(__GNUC__)
+    } SuIntraPacketRtc;
+#else
+    } __attribute__ ((packed)) SuIntraPacketRtc;
+#endif
 
 
 /*
@@ -93,37 +152,47 @@ typedef struct
  * --------------------
  */
 
-I106_DLL_DECLSPEC EnI106Status I106_CALL_DECL 
+EnI106Status I106_CALL_DECL 
     enI106_SetRelTime(int              iI106Ch10Handle,
                       SuIrig106Time  * psuTime,
                       uint8_t          abyRelTime[]);
 
-I106_DLL_DECLSPEC EnI106Status I106_CALL_DECL 
+EnI106Status I106_CALL_DECL 
     enI106_Rel2IrigTime(int                  iI106Ch10Handle,
                         uint8_t              abyRelTime[],
                         SuIrig106Time      * psuTime);
 
-I106_DLL_DECLSPEC EnI106Status I106_CALL_DECL 
+EnI106Status I106_CALL_DECL 
     enI106_Irig2RelTime(int              iI106Ch10Handle,
                         SuIrig106Time  * psuTime,
                         uint8_t          abyRelTime[]);
 
 // Warning - array to int / int to array functions are little endian only!
 
-I106_DLL_DECLSPEC void I106_CALL_DECL 
+void I106_CALL_DECL 
     vLLInt2TimeArray(int64_t * pllRelTime,
                      uint8_t   abyRelTime[]);
 
 
-I106_DLL_DECLSPEC void I106_CALL_DECL 
+void I106_CALL_DECL 
     vTimeArray2LLInt(uint8_t   abyRelTime[],
                      int64_t * pllRelTime);
 
 
-I106_DLL_DECLSPEC EnI106Status I106_CALL_DECL 
+EnI106Status I106_CALL_DECL 
     enI106_SyncTime(int     iI106Ch10Handle,
-                    int     bRequireSync,
-                    int     iTimeLimit);
+                    int     bRequireSync,       // Require external time sync
+                    int     iTimeLimit);        // Max scan ahead time in seconds, 0 = no limit
+
+EnI106Status I106_CALL_DECL 
+    enI106Ch10SetPosToIrigTime(int iI106Ch10Handle, SuIrig106Time * psuSeekTime);
+
+
+// General purpose time utilities
+// ------------------------------
+
+// Convert IRIG time into an appropriate string
+char * IrigTime2String(SuIrig106Time * psuTime);
 
 // IT WOULD BE NICE TO HAVE SOME FUNCTIONS TO COMPARE 6 BYTE
 // TIME ARRAY VALUES FOR EQUALITY AND INEQUALITY
@@ -131,7 +200,7 @@ I106_DLL_DECLSPEC EnI106Status I106_CALL_DECL
 // This is handy enough that we'll go ahead and export it to the world
 // HMMM... MAYBE A BETTER WAY TO DO THIS IS TO MAKE THE TIME VARIABLES
 // AND STRUCTURES THOSE DEFINED IN THIS PACKAGE.
-I106_DLL_DECLSPEC time_t I106_CALL_DECL 
+time_t I106_CALL_DECL 
     mkgmtime(struct tm * psuTmTime);
 
 #ifdef __cplusplus
