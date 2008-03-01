dc0bf5293063eac0be4d180383aa48f2e6f862e4
bbaggerman <bbaggerman@133a2ce8-d050-0410-a33c-a8f588bb09b4>
2008-03-01 04:12:34 +0000

    Some .NET stuff

--- src/config.h
+++ src/config.h
@@ -33,12 +33,6 @@
  (including negligence or otherwise) arising in any way out of the use 
  of this software, even if advised of the possibility of such damage.
 
- Created by Bob Baggerman
-
- $RCSfile: config.h,v $
- $Date: 2006-11-30 02:34:22 $
- $Revision: 1.2 $
-
  ****************************************************************************/
 
 #ifndef _config_h_
@@ -48,18 +42,39 @@
 extern "C" {
 #endif
 
+// .NET 2005 C++ wants structures that are passed as function parameters to be declared
+// as public.  .NET 2003 and native C pukes on that. C++ Interop doesn't seem to care.
+//  Grrrr...  Just define out PUBLIC for now but leave in the macro logic in case I want
+// to revisit this someday.  Yeah, right!
+//#if _MSC_VER >= 1400
+//#define PUBLIC public
+//#else
+#define PUBLIC
+//#endif
+
+// .NET 2005 (and probably earlier, but I'm not sure) define time_t to be a 64 bit value.
+// And by default, all the CRT time routines are the 64 bit versions.  For best portability,
+// time_t is assumed to be a 32 bit value.  The following #define tells .NET to use 32 bits
+// as the default time_t size.  This needs to be set in the project properties.  This forces
+// a puke if it isn't set.
+#if _MSC_VER >= 1400
+  #if !defined(_USE_32BIT_TIME_T)
+  #pragma message("WARNING - '_USE_32BIT_TIME_T' not set!")
+  #endif
+#endif
 
 /* The POSIX caseless string compare is strcasecmp(). MSVC uses the
  * non-standard stricmp(). Fix it up with a macro if necessary
  */
 
 #if defined(_MSC_VER) 
-#define strcasecmp(s1, s2)          stricmp(s1, s2)
-#define strncasecmp(s1, s2, n)      strnicmp(s1, s2, n)
+#define strcasecmp(s1, s2)          _stricmp(s1, s2)
+#define strncasecmp(s1, s2, n)      _strnicmp(s1, s2, n)
+#pragma warning(disable : 4996)
 #endif
 
+#define I106_CALL_DECL
 
- 
 #ifdef __cplusplus
 }
 #endif
