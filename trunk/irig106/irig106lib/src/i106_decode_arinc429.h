eb9ba57b0f13d0562b7cd2824d25bbb29ea4905e
bbaggerman <bbaggerman@133a2ce8-d050-0410-a33c-a8f588bb09b4>
2008-06-29 14:49:50 +0000

    ARINC data packet support

---
+++ trunk/irig106/irig106lib/src/i106_decode_arinc429.h
@@ -0,0 +1,119 @@
+/****************************************************************************
+
+ i106_decode_arinc429.h - 
+
+ Copyright (c) 2008 Irig106.org
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without 
+ modification, are permitted provided that the following conditions are 
+ met:
+
+   * Redistributions of source code must retain the above copyright 
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright 
+     notice, this list of conditions and the following disclaimer in the 
+     documentation and/or other materials provided with the distribution.
+
+   * Neither the name Irig106.org nor the names of its contributors may 
+     be used to endorse or promote products derived from this software 
+     without specific prior written permission.
+
+ This software is provided by the copyright holders and contributors 
+ "as is" and any express or implied warranties, including, but not 
+ limited to, the implied warranties of merchantability and fitness for 
+ a particular purpose are disclaimed. In no event shall the copyright 
+ owner or contributors be liable for any direct, indirect, incidental, 
+ special, exemplary, or consequential damages (including, but not 
+ limited to, procurement of substitute goods or services; loss of use, 
+ data, or profits; or business interruption) however caused and on any 
+ theory of liability, whether in contract, strict liability, or tort 
+ (including negligence or otherwise) arising in any way out of the use 
+ of this software, even if advised of the possibility of such damage.
+
+ Created by Bob Baggerman
+
+ ****************************************************************************/
+
+#ifndef _I106_DECODE_ARINC429_H
+#define _I106_DECODE_ARINC429_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Macros and definitions
+ * ----------------------
+ */
+
+
+/*
+ * Data structures
+ * ---------------
+ */
+
+#if defined(_MSC_VER)
+#pragma pack(push,1)
+#endif
+
+// Channel specific data word
+// --------------------------
+
+typedef struct Arinc429F0_ChanSpec_S
+    {
+    uint32_t    uMsgCount       : 16;      // Message count
+    uint32_t    Reserved        : 16;      //
+#if !defined(__GNUC__)
+    } SuArinc429F0_ChanSpec;
+#else
+    } __attribute__ ((packed)) SuArinc429F0_ChanSpec;
+#endif
+
+// Intra-message header
+typedef struct Arinc429F0_Header_S
+    {
+    uint32_t    uGapTime        : 20;      // Gap Time
+    uint32_t    Reserved        :  1;      // 
+    uint32_t    uBusSpeed       :  1;      // Bus Speed
+    uint32_t    bParityError    :  1;      // Parity Error
+    uint32_t    bFormatError    :  1;      // Data type
+    uint32_t    uBusNum         :  8;      // Bus number
+#if !defined(__GNUC__)
+    } SuArinc429F0_Header;
+#else
+    } __attribute__ ((packed)) SuArinc429F0_Header;
+#endif
+
+// ARINC 429 data format
+typedef struct Arinc429F0_Data_S
+    {
+    uint32_t    uLabel          :  8;      // Label
+    uint32_t    uSDI            :  2;      // Source/Destination Identifiers
+    uint32_t    uData           : 19;      // Data
+    uint32_t    uSSM            :  2;      // Sign/Status Matrix
+    uint32_t    uParity         :  1;      // Parity
+#if !defined(__GNUC__)
+    } SuArinc429F0_Data;
+#else
+    } __attribute__ ((packed)) SuArinc429F0_Data;
+#endif
+
+
+#if defined(_MSC_VER)
+#pragma pack(pop)
+#endif
+
+/*
+ * Function Declaration
+ * --------------------
+ */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
