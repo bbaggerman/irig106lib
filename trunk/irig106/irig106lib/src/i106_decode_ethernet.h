00ad44a0f16326e9fa3335e9005f475d699621d3
bbaggerman <bbaggerman@133a2ce8-d050-0410-a33c-a8f588bb09b4>
2008-06-29 14:49:04 +0000

    Ethernet packet  support

---
+++ trunk/irig106/irig106lib/src/i106_decode_ethernet.h
@@ -0,0 +1,115 @@
+/****************************************************************************
+
+ i106_decode_ethernet.h - 
+
+ Copyright (c) 2008 Irig106.org
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without 
+ modification, are permitted provided that the following conditions are 
+ met:
+
+   * Redistributions of source code must retain the above copyright 
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright 
+     notice, this list of conditions and the following disclaimer in the 
+     documentation and/or other materials provided with the distribution.
+
+   * Neither the name Irig106.org nor the names of its contributors may 
+     be used to endorse or promote products derived from this software 
+     without specific prior written permission.
+
+ This software is provided by the copyright holders and contributors 
+ "as is" and any express or implied warranties, including, but not 
+ limited to, the implied warranties of merchantability and fitness for 
+ a particular purpose are disclaimed. In no event shall the copyright 
+ owner or contributors be liable for any direct, indirect, incidental, 
+ special, exemplary, or consequential damages (including, but not 
+ limited to, procurement of substitute goods or services; loss of use, 
+ data, or profits; or business interruption) however caused and on any 
+ theory of liability, whether in contract, strict liability, or tort 
+ (including negligence or otherwise) arising in any way out of the use 
+ of this software, even if advised of the possibility of such damage.
+
+ Created by Bob Baggerman
+
+ ****************************************************************************/
+
+#ifndef _I106_DECODE_ETHERNET_H
+#define _I106_DECODE_ETHERNET_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Macros and definitions
+ * ----------------------
+ */
+
+
+/*
+ * Data structures
+ * ---------------
+ */
+
+#if defined(_MSC_VER)
+#pragma pack(push,1)
+#endif
+
+// Channel specific data word
+// --------------------------
+
+typedef struct EthernetF0_ChanSpec_S
+    {
+    uint32_t    uNumFrames      : 16;      // Number of frames
+    uint32_t    Reserved1       : 12;
+    uint32_t    uFormat         :  4;      // Format of frames
+#if !defined(__GNUC__)
+    } SuEthernetF0_ChanSpec;
+#else
+    } __attribute__ ((packed)) SuEthernetF0_ChanSpec;
+#endif
+
+// Intra-message header
+typedef struct EthernetF0_Header_S
+    {
+    uint8_t     aubyIntPktTime[8];         // Reference time
+    uint32_t    uDataLen        : 14;      // Data length
+    uint32_t    Reserved1       :  2;      // 
+    uint32_t    uNetID          :  8;      // Network identifier
+    uint32_t    uSpeed          :  4;      // Ethernet speed
+    uint32_t    uContent        :  2;      // Captured data content
+    uint32_t    bFrameError     :  1;      // Frame error
+    uint32_t    Reserved2       :  1;      // 
+#if !defined(__GNUC__)
+    } SuEthernetF0_Header;
+#else
+    } __attribute__ ((packed)) SuEthernetF0_Header;
+#endif
+
+#if defined(_MSC_VER)
+#pragma pack(pop)
+#endif
+
+/*
+ * Function Declaration
+ * --------------------
+ */
+
+EnI106Status I106_CALL_DECL 
+    enI106_Decode_Tmats(SuI106Ch10Header * psuHeader,
+                        void             * pvBuff,
+                        SuTmatsInfo      * psuTmatsInfo);
+
+void I106_CALL_DECL 
+    enI106_Free_TmatsInfo(SuTmatsInfo    * psuTmatsInfo);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
