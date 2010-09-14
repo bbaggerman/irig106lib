2e21009b62bc1aba60bf4ed38531d34ae1e22526
bbaggerman <bbaggerman@133a2ce8-d050-0410-a33c-a8f588bb09b4>
2010-09-14 02:43:46 +0000

    Moved a bunch of packet structures to the i106_decode_index.h file

--- src/i106_index.h
+++ src/i106_index.h
@@ -38,18 +38,6 @@
 #ifndef _I106_INDEX_H
 #define _I106_INDEX_H
 
-#include <string.h>
-#include <stdio.h>
-#include <io.h>
-#include <stdlib.h>
-#include <time.h>
-
-#include "config.h"
-#include "stdint.h"
-#include "Irig106Ch10.h"
-#include "i106_time.h"
-#include "i106_decode_time.h"
-
 #ifdef __cplusplus
 namespace Irig106 {
 extern "C" {
@@ -68,45 +56,10 @@ extern "C" {
 
 typedef struct
     {
-    uint32_t    uIdxEntCount    : 16;   // Total number of indexes
-    uint32_t    uReserved       : 13;
-    uint32_t    bIntraPckHdr    :  1;   // Intra-packet header present
-    uint32_t    bFileSize       :  1;   // File size present
-    uint32_t    uIndexType      :  1;   // Index type
-#if !defined(__GNUC__)
-    } SuIndex_ChanSpec;
-#else
-    } __attribute__((packed)) SuIndex_ChanSpec;
-#endif
-
-typedef struct
-    {
     uint64_t        ui64RelTime;
     SuIrig106Time   suIrigTime;
     uint64_t        ui64FileOffset;
-    } SuIndexTableNode;
-
-
-typedef struct
-    {
-    int iCurrent;
-    } SuRootIndexPacket;
-
-// This struct holds the data of the first 4 bytes in the node index entry layout
-typedef struct 
-    {
-    uint32_t    uChannelID      : 16;
-    uint32_t    uDataType       :  8;
-    uint32_t    uReserved       :  8;
-    //uint32_t u32Offset_LSLW;
-    //uint32_t u32Offset_MSLW;
-    uint64_t    uOffset;
-#if !defined(__GNUC__)
-    } SuIndex_Data;
-#else
-    } __attribute__((packed)) SuIndex_Data;
-#endif
-
+    } SuPacketIndex;
 
 
 /*
@@ -124,22 +77,7 @@ EnI106Status I106_CALL_DECL enIndexPresent(int iHandle, int * bFoundIndex);
 
 EnI106Status I106_CALL_DECL enReadIndexes(int iHandle);
 
-#if 0
-
-EnI106Status I106_CALL_DECL SaveIndexTable(char* strFileName);
-
-EnI106Status I106_CALL_DECL ProcessRootIndexPackBody( int iHandle, SuI106Ch10Header suHeader );
-
-EnI106Status I106_CALL_DECL ProcessNodeIndexPack(int iHandle, uint64_t u64NodeIndexOffset);
-
-EnI106Status I106_CALL_DECL ProcessNodeIndexPackBody(int iHandle, SuI106Ch10Header suHeader);
-
-EnI106Status ReallocIndexTable();
-
-void DeleteIndexTable();
-
-EnI106Status ReadTimeDataPacketBody(SuI106Ch10Header suTimeDataHeader);
-#endif
+//EnI106Status I106_CALL_DECL SaveIndexTable(char* strFileName);
 
 #ifdef __cplusplus
 }
